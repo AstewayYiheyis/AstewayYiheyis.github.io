87bde642fbfed2a272d46b8b8a21472d3e4ec1e0
AstewayYiheyis <90155375+AstewayYiheyis@users.noreply.github.com>
2021-11-20 03:12:57 -0600

    Delete tip_calculator directory

--- tip_calculator/tip.js
+++
@@ -1,9 +0,0 @@
-function calcTip() {
-    // Enter your code here to make Tip Calculator work
-    let subtotal = document.getElementById("subtotal").value;
-    let tipRate = document.getElementById("tip").value/100;
-    let tip = tipRate * subtotal;
-    let total = parseFloat(subtotal) + parseFloat(tip);
-
-    document.getElementById("total").innerHTML = total;
-}
